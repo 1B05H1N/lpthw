3afb2b15eb21c033639efac3dabb1a7e2ad0594b
ibo <41275178+1B05H1N@users.noreply.github.com>
2021-06-16 08:28:14 -0700

    Updates
    Exercise 10 through 15.

---
+++ ex11.py
@@ -0,0 +1,8 @@
+print("How old are ya?"),
+age=input()
+print("How tall are ya?"),
+height=input()
+print("How much do you weigh?"),
+weight=input()
+
+print("So, you're %r old, %r tall and %r heavy. Wooow!." % (age, height, weight))
---
+++ ex12.py
@@ -0,0 +1,9 @@
+age=input("How old are ya?")
+height=input("How tall are ya?")
+weight=input("How much do you weigh?")
+
+print("So, you're %r old, %r tall and %r heavy. Wooow!." % (age, height, weight))
+
+"""
+python -m pydoc <file>
+"""
