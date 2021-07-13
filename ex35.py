c81e003d117db0b596d89b5a4bbe94e0a64f90bb
ibo <41275178+1B05H1N@users.noreply.github.com>
2021-07-13 06:05:24 -0700

    Updates

--- ex35.py
+++ ex35.py
@@ -26,14 +26,7 @@ def bear_room():
     while True:
         next = input("> ")
         
-        if next == ("take honey"):
-            dead("The bear looks at you then slaps your face off.")
-        elif next == ("taunt bear") and not bear_moved:
-            print("The bear has moved from the door. You can go through it now.")
-            bear_moved = True
-        elif next == ("taunt bear") and bear_moved:
-            dead("The bear gets pissed off and chews your leg off.")
-        elif next == ("open door") and bear_moved:
+       i== ("open door") and bear_moved:
             gold_room()
         else:
             print("I got no idea what that means.")
